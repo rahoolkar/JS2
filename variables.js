c2d07a7c9598aaad283d50d4e38271385df3cda4
rahul kar <batman30jan2002@gmail.com>
2025-01-08 17:35:31 +0530

    lecture 4 done

---
+++ variables.js
@@ -0,0 +1,11 @@
+const a = 90 ;
+//const variables cannot be re-declared and re-assigned
+let b = 80 ;
+//let variables cannot be re-declared but can be re-assigned
+var c = 60 ;
+//var variables can be re-declared and can be re-assigned
+d = 10;
+//d is a global variable
+
+
+//we have stopped using var because it is a global variablr and it can be easily changed from any function.
