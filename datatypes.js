0ba9162e2d6214a42da147866130d137d96669a7
rahul kar <batman30jan2002@gmail.com>
2025-01-08 18:29:05 +0530

    lecture 5 done

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //this is used to tell js that treat this code as newer version of code
+
+//data types in js -> primitive datatypes & non-primitive datatypes
+
+//primitive datatypes-> nn bb ss u
+
+let number = 123; //koi bhi number value
+let string = "rahul"; //combination of letters
+let boolean = true; //true/false 
+let bigInteger = 1234n; // BigInt(1234567890123456789012345); 
+let nullV = null; //null is a standalone value //isme null value hai
+let undef = undefined ; // let und ; //isme koi value hi nhi aayi hai 
+let symbol = Symbol("foo"); 
+
+console.log(typeof null); //null ka type object hota hai
+console.log(typeof undef); //undefined ka type undefined hi aata hai
+
