01f348273ab32af2d24e44ef167e6577739b7d24
rahoolkar <batman30jan2002@gmail.com>
2025-01-13 13:27:06 +0530

    lecture 12

---
+++ Number.js
@@ -0,0 +1,21 @@
+//defining a number 
+let number = 234;
+
+//defining a number through constructor
+let score = new Number(100);
+//we can see all the properties and methods that we can use with number 
+
+let num = 899.90765;
+
+//methods with number
+console.log(num.toFixed(2)); //decimal ke baad kitna values show karne hai
+console.log(num.toPrecision(4)); //total kitne digits shoe karega
+console.log(num.toString());
+console.log(num.toLocaleString());
+
+//Number Object Methods
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+
