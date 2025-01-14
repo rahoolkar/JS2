85ebbdf3c5f6e97c0aadbf5e216f95975bdbb10d
rahoolkar <batman30jan2002@gmail.com>
2025-01-15 00:49:50 +0530

    lecture 17

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
 //singleton method
+let obj = new Object({name:"udayan",age:67}); //singleton method of returning a object
+console.log(obj); 
 
 //object literals
 let dictionary ={
@@ -36,4 +38,42 @@ console.log(dictionary);
 console.log(dictionary.hello); // this is give me the refernce to the functionn
 dictionary.goodmorning(); //calling the function
 
-//
+//Object Methods
+Object.freeze(obj);
+
+let newObj = Object.assign({},dictionary,obj);
+console.log(newObj);
+
+let keys = Object.keys(dictionary);
+console.log(keys); //array return hota hai
+
+let values = Object.values(dictionary);
+console.log(values); //array return hota hai
+
+//using spread operator with objects 
+let newObj2 = {...dictionary,...obj};
+console.log(newObj2);
+
+//Nesting objects 
+let regularUser = {
+    dateOfJoining : {
+        time:{
+            minutes:56,
+            hours:17,
+            seconds:23
+        },
+        fulldate : {
+            year:2020,
+            month:12,
+            day:5
+        }
+    },
+    name:"rahulkar",
+    age:67
+}
+
+//accessing the values in the nested objects
+console.log(regularUser.dateOfJoining?.time?.hours);
+
+//asking the object if it has the mentioned property
+console.log(obj.hasOwnProperty("id"));
