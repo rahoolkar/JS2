c2bde27a0c86ffc38f57081f3c115d60b0a6b121
rahoolkar <batman30jan2002@gmail.com>
2025-01-15 17:04:57 +0530

    lecture 19

---
+++ functions.js
@@ -0,0 +1,14 @@
+//defining a function //function declaration and function statement
+function goodmorning(){
+    console.log("good morning");
+}
+
+//calling the function 
+goodmorning(); 
+
+function sum(para1,para2){ //parameters 
+    return para1+para2; //code after return statement will be ignored
+}
+
+let add = sum(2,4); //arguments //jab hum arguements me kuch bhi nahi pass karte hai toh undefined aata hai
+console.log(add); 
