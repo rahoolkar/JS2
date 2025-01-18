2ffa02f2b2ed5f078f21ba9d257e757e4525e3fa
rahoolkar <batman30jan2002@gmail.com>
2025-01-18 15:15:38 +0530

    lecture 28

---
+++ while.js
@@ -0,0 +1,17 @@
+//while loop 
+
+let counter = 1 ; 
+while(counter<=15){
+    console.log(`counter value is ${counter}`);
+    counter = counter + 2;
+}
+
+
+//do-while loop
+//pehle kaam hoga then condition check hogi
+
+let iterator = 1;
+do{
+    console.group(`iterator value is ${iterator}`);
+    iterator = iterator + 3;
+}while(iterator<20);
