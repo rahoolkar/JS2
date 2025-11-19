e33e47cff6466aecae15d929346c1822919abb1b
rahoolkar <batman30jan2002@gmail.com>
2025-11-19 15:19:51 +0530

    lecture 5 done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict" //treat all Js code as newer version
+
+let age = 90 ;
+let x = null;
+
+//datatypes in javascript
+
+//Primitive Data Type
+
+//number
+//null -> it is a datatype and a standalone value 
+//boolean
+//bigInt
+//string
+//symbol
+//undefined -> it is a placeholder for the future value
+
+//Non Primitive Data Type
+
+//object
+
+console.log(typeof "rahulkar");
+console.log(typeof age);
+console.log(typeof x); //object
+console.log(typeof undefined); //undefined
+
