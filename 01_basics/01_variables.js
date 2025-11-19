c187af18c98da21ca55d4081ef2d1b3f4824fc17
rahoolkar <batman30jan2002@gmail.com>
2025-11-19 14:47:12 +0530

    lecture 4 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 909090;
+let accountName = "Rahul Kar"
+var accountPassword = "rahul@1001"
+accountEmail = "rahul@google.com"
+let accountState; //undefined
+
+console.table([accountId,accountName,accountPassword,accountEmail,accountState]);
