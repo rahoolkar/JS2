b93bf0b27ce08afaf9574b33c9994a27dfaac46a
rahoolkar <batman30jan2002@gmail.com>
2025-11-21 14:44:24 +0530

    lecture 9

---
+++ datatype-summary.js
@@ -0,0 +1,9 @@
+//Datatypes in JavaScript - Javascript data types are mainly divided how they are stored in the memory and how we access the value from the memory. 
+
+// Primitive Datatype - Call by value - Number, String, Symbol, Null, Boolean, BigInt, Undefined.
+
+// Non Primitive Datatype - Call by reference - Arrays, Objects, Functions. 
+
+//Javascript is a loosely typed language because we never define what type of data will be assigned to the variable when we initialize it.
+
+//JavaScript is a dynamically typed language. 
