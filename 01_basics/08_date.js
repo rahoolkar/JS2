455f6b58a4b484b7c9430a9f50e6480be9f22646
rahoolkar <batman30jan2002@gmail.com>
2025-11-27 15:23:27 +0530

    lecture 13 done

---
+++ 01_basics/08_date.js
@@ -0,0 +1,15 @@
+const mydate = new Date();
+
+console.log(typeof mydate); // -> object
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+const myCreatedDate = new Date(2025,0,30);
+const myCreatedDateWithTime = new Date(2025,0,30,1,12);
+
+console.log(myCreatedDate);
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleDateString());
