f3c95a0be26a847605fc619de1a8b92072655bf9
rahoolkar <batman30jan2002@gmail.com>
2025-12-02 17:35:36 +0530

    lecture 21 done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+//variables defined in the global scope
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+{
+  // variables defined in the block scope
+
+  var a = 100;
+  let b = 200;
+  const c = 300;
+
+  console.log("Inner", a);
+  console.log("Inner", b);
+  console.log("Inner", c);
+}
+
+console.log("Outer", a);
+console.log("Outer", b);
+console.log("Outer", c);
+
+//value of varible defined by var changes because it is defined in the global scope. This is main problem with using the var.
+//outer varibles should be accessed in the inner scope but inner variables should not be accessed in the outer scope. 
