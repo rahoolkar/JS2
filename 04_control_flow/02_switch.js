73e36636f2233dae5601249c148843e500620909
rahoolkar <batman30jan2002@gmail.com>
2025-12-06 00:55:36 +0530

    lecture 26 done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,17 @@
+//switch statement in javascript
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+    
+  case 2:
+    console.log("February");
+    break;
+
+  default:
+    console.log("this is the default case");
+    break;
+}
