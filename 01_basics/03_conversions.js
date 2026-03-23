9f9fe01db085d5a64e4b47eefc610100b93d539b
rahoolkar <batman30jan2002@gmail.com>
2026-03-24 00:55:30 +0530

    lecture 7 done

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -24,3 +24,19 @@ let emptyString = "";
 let convertedEmptyString = Number(emptyString);
 console.log(convertedEmptyString); // 0 - intentional empty space is always a zero
 console.log(typeof convertedEmptyString); //number
+
+
+//conversions to string
+let num = 0;
+let convertedNum = String(num);
+console.log(convertedNum);// "90"
+console.log(typeof convertedNum) //string
+
+//conversions to boolean
+let str = 1 ;
+let convertedStr = Boolean(str);
+console.log(convertedStr);
+console.log(typeof convertedStr); 
+
+
+// undefinded, intentional empty space are always false
---
+++ 01_basics/09_coversions&operations.js
@@ -0,0 +1,13 @@
+let str1 = 1;
+let str2 = "2";
+let str3 = str1 + str2;
+
+console.log(str3);
+console.log(typeof str3);
+
+
+//prefix and postfix precedence operator
+
+let gameScore = 90;
+
+console.log(++gameScore);
