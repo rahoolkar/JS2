ae26fab2895f6a555bde14585a2a0d0fc953b7e3
rahoolkar <batman30jan2002@gmail.com>
2026-03-25 14:03:14 +0530

    lecture 9 done

--- datatype-summary.js
+++ datatype-summary.js
@@ -1,9 +1,34 @@
-//Datatypes in JavaScript - Javascript data types are mainly divided how they are stored in the memory and how we access the value from the memory. 
+//Datatypes in JavaScript - Javascript data types are mainly divided how they are stored in the memory and how we access the value from the memory.
 
-// Primitive Datatype - Call by value - Number, String, Symbol, Null, Boolean, BigInt, Undefined.
+// Primitive Datatype - Call by value - Number, String, Symbol, Null, Boolean, BigInt, Undefined. - means when we copy the value of the variable we get the copy of the value of the variable not the reference
 
-// Non Primitive Datatype - Call by reference - Arrays, Objects, Functions. 
+let a = 90; // returns number as type
+let b = null; //returns object as type
+let c = "rahul"; //returns string as type
+let d = Symbol("1234"); //returns symbol as type
+let e = true; //returns boolean as type
+let f = BigInt("1233444556688282929"); //let f = 1234567890n; //returns bigint as type
+let g = undefined; //let g ; // returns undefined as type
+
+// console.log(typeof d);
+
+// Non Primitive Datatype - Call by reference - Arrays, Objects, Functions. - means when we copy the variable then we get the reference of memory where the varibale is stored
+
+let array = ["delhi", "bombay", "jaipur"];
+
+function print() {
+  console.log("hello world");
+}
+
+let obj = {
+  name: "rahulkar",
+  age: 24,
+  city: "rohtak",
+};
+
+console.log(obj);
+console.log(typeof obj);
 
 //Javascript is a loosely typed language because we never define what type of data will be assigned to the variable when we initialize it.
 
-//JavaScript is a dynamically typed language. 
+//JavaScript is a dynamically typed language.
