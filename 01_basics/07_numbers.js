35895866ae650a845782797f3aa6fe33d026faba
rahoolkar <batman30jan2002@gmail.com>
2026-03-25 16:55:36 +0530

    lecture 13

--- 01_basics/07_numbers.js
+++ 01_basics/07_numbers.js
@@ -1,4 +1,4 @@
-const age = 90 ;
+const age = 90;
 const score = 100.6788; // its a primitive number value
 const balance = new Number(3456.234); //its an object containing number value
 
@@ -15,19 +15,19 @@ console.log(Number.MIN_VALUE);
 
 //Maths object in javascript
 
-console.log(Math.PI);
-console.log(Math.abs(-4));
-console.log(Math.round(34.67));
-console.log(Math.ceil(1.1));
-console.log(Math.floor(1.9));
-console.log(Math.min(45,67));
-console.log(Math.max(56,34,56,90));
-console.log((Math.random()*10) + 1); //0-1
-
+console.log(Math.PI); //3.14
+console.log(Math.min(12, 24, 36, 48)); //12
+console.log(Math.max(1, 2, 4, 5, 56)); //56
+console.log(Math.abs(-44)); //44
+console.log(Math.round(78.89)); //79
+console.log(Math.floor(49.9)); //49
+console.log(Math.ceil(45.1)); //46
+console.log(Math.pow(2, 3)); //8
+console.log(Math.sqrt(49)); //7
 
 //for range 10-20
 
-let min = 10; 
+let min = 10;
 let max = 20;
 
-console.log(Math.floor(Math.random() * (max - min +1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
