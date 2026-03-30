d56e5ee750fa7b350d22e7a68a15125ddee60014
rahoolkar <batman30jan2002@gmail.com>
2026-03-30 16:14:28 +0530

    lecture 23

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -17,7 +17,7 @@ console.log(this);
 //in the node enviroment this points to an empty object and in the browser environment the this points to the global object.
 
 function chai(){
-    console.log(this); // here (this) points to the global object.
+    console.log(this); // here (this) points to the global object. //this object points to the bhar vala object in the case of normal function initialization
 }
 
 chai();
@@ -25,7 +25,7 @@ chai();
 //now when it comes to the arrow function, this keyword points to an empty object
 
 const arrow = () => {
-    console.log(this);
+    console.log("this in arrow function" ,this);
 }
 
 arrow();
@@ -36,7 +36,7 @@ const sum = (a,b) => {
     return a + b;  //explicit return 
 }
 
-const mult = (a,b) => a*b ;  //implicit return
+const mult = (a,b) => (a*b) ;  //implicit return
 
 console.log(sum(3,4));
 console.log(mult(5,2));
