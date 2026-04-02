9e258feba106dcf405d57420714f0c17b0998d1c
rahoolkar <batman30jan2002@gmail.com>
2026-04-02 16:15:48 +0530

    lecture31

---
+++ functionsTrick.js
@@ -0,0 +1,41 @@
+//q1
+
+// var x = 1; 
+
+// function a(){
+//     var x = 100; 
+//     console.log(x);
+// }
+
+// function b(){
+//     var x = 1000;
+//     console.log(x);
+// }
+
+// a();
+// b();
+// console.log(x);
+
+//q2
+
+// var x = 90;
+
+// function a(){
+// 	var x = 100;
+// 	console.log(x);
+// }
+
+// console.log(x);
+// a();
+// console.log(x);
+
+//q3
+
+var x = 1 ;
+
+if(true){
+    var x = 10;
+    console.log(x);
+}
+
+console.log(x);
