668457a2fba3ea3c8ad9f206d330c25c0b50af8e
rahoolkar <batman30jan2002@gmail.com>
2026-04-21 16:57:35 +0530

    error handling

--- fetch.js
+++ fetch.js
@@ -3,13 +3,19 @@
 //fetch api is used to make api calls from js and it is a async function *returns a promise
 
 async function getData() {
-  const response = await fetch("https://api.github.com/users/rahoolkar"); //it will return a promise and this promise has a response object as result and we have to use .json() to convert it into json
+  try {
+    const response = await fetch("https://api.github.com/users/rahoolkar"); //it will return a promise and this promise has a response object as result and we have to use .json() to convert it into json
 
-  const json = await response.json(); //.json() is a async function that is use to convert the response body to json
+    const json = await response.json(); //.json() is a async function that is use to convert the response body to json
 
-  console.log(json);
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 console.log("start");
 getData();
+
+//getData().catch((error)=>{ console.log(error) }); //this is also a way to handle error in sync await
 console.log("end");
