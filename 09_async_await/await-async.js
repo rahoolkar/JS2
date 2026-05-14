d757f2442f6ae7a4a15a49d79c157354665a83e0
rahoolkar <batman30jan2002@gmail.com>
2026-05-14 23:44:24 +0530

    async await

--- 09_async_await/await-async.js
+++ 09_async_await/await-async.js
@@ -1,8 +1,15 @@
-const promise = new Promise(function (resolve, reject) {
-  console.log("inside the promise");
+const promise1 = new Promise(function (resolve, reject) {
+  console.log("inside the promise1");
   setTimeout(() => {
     resolve(200);
   }, 10000);
+});
+
+const promise2 = new Promise(function (resolve, reject) {
+  console.log("inside promise 2");
+  setTimeout(function () {
+    resolve(300);
+  }, 5000);
 });
 
 // function getData() {
@@ -14,8 +21,19 @@ const promise = new Promise(function (resolve, reject) {
 // getData();
 
 async function handleData() {
-  let value = await promise;
-  console.log(value);
+  console.log("start");
+  const value1 = await promise1;
+  console.log("promise1 resolved");
+  console.log(value1);
+
+  const value2 = await promise2;
+  console.log("promise2 resolved");
+  console.log(value2);
+
+  console.log("end");
 }
 
 handleData();
+
+console.log("global line ending");
+console.log("sayonara");
